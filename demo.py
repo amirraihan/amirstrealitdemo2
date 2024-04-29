6f4a4423209f44bf1025c60f09bf7de9831196a0
Amir Raihan <76759143+amirraihan@users.noreply.github.com>
2024-04-29 15:36:12 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,3 +12,9 @@ plt.plot(data["x"][:subset_size], data["y"][:subset_size], "-o")
 st.pyplot(plt)
 # Add a text input for comments or notes
 notes = st.text_area("Add your comments or notes here")
+
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
+Third-party components
+
